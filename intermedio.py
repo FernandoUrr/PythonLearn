83818e38e296542df5734bed1001e6b2051aa205
FDUP <furruchurto@uninorte.edu.co>
2021-08-30 12:47:36 -0500

    Nested dictionaries

--- intermedio.py
+++ intermedio.py
@@ -1,5 +1,21 @@
-def cualquier_funcion():
+def nested_dictionaries_and_lists():
     pass
 
+def run():
+    menu = """
+    Estos son los algoritmos de intermedio
+    1. Nested dictionaries and lists
+    2. 
+    3. 
+    4.
+    5.
+    6.
+    7.
+    Selecciona una opción:
+    """
+    a = int(input(menu))
+    if a == 1:
+        return nested_dictionaries_and_lists()
+
 if __name__ == '__main__':
-    cualquier_funcion()
+    run()
