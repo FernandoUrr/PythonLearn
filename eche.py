244645bfbad1be59ac1a953840db241493299a04
FDUP <furruchurto@uninorte.edu.co>
2021-09-13 18:11:15 -0500

    Amen

---
+++ eche.py
@@ -0,0 +1,12 @@
+def run():
+    list = [1, 2, 3, 5, 3, 2, 4, 5, 5, 4]
+    def not_duplicates(list):
+        list_p = list
+        some_list = [i for i in list if i not in list_p]
+        
+
+
+
+
+if __name__ == '__main__':
+    run()
