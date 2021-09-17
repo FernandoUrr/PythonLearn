7f0c2d03987310853ebec6fe175e7c249a45ae01
FDUP <furruchurto@uninorte.edu.co>
2021-09-17 13:21:59 -0500

    Se agrega lo nuevo

--- Macros.vb
+++ Macros.vb
@@ -103,3 +103,122 @@ Private Sub Worksheet_Change(ByVal Target As Range)
         End If
     End If
 End Sub
+
+'Este es uno con mejores actualizaciones, capaz de mejorar
+'El Keyword Call es capaz de llamar una macro la cual estará explicada más debajo
+Private Sub Worksheet_Change(ByVal Target As Range)
+If Application.Intersect(Target, Range("I15")) Is Nothing Then
+    Exit Sub
+Else
+    e = "Etapa 1: Evaluación AS IS y definición TO BE de Procedimientos, instructivos y perfiles"
+    a = "Etapa 2: Parametrización Conjunta"
+    n = "Etapa 3: Pruebas de parametrización"
+    b = "Etapa 4: Cargue de información"
+    l = "Etapa 5: Actualización de documentos: Procedimientos, instructivos, anexos y perfiles"
+    c = "Etapa 6: Socialización y capacitación"
+    d = Target.Value
+
+    If (d = l) Or (d = a) Then
+        For i = 24 To 31
+            Worksheets("Control de Etapas").Rows(i).Hidden = False
+        Next
+        For i = 26 To 31
+            Worksheets("Control de Etapas").Rows(i).Hidden = True
+        Next
+        Range("G20:M31").Select
+        Selection.ClearContents
+        Range("I15").Select
+        Call Bring_Information
+    Else
+        If (d = b) Or (d = c) Then
+            For i = 24 To 31
+                Worksheets("Control de Etapas").Rows(i).Hidden = False
+            Next
+            For i = 24 To 31
+                Worksheets("Control de Etapas").Rows(i).Hidden = True
+            Next
+            Range("G20:M31").Select
+            Selection.ClearContents
+            Range("I15").Select
+            Call Bring_Information
+        Else
+            If (d = n) Then
+                For i = 24 To 31
+                    Worksheets("Control de Etapas").Rows(i).Hidden = False
+                Next
+                For i = 30 To 31
+                    Worksheets("Control de Etapas").Rows(i).Hidden = True
+                Next
+                Range("G20:M31").Select
+                Selection.ClearContents
+                Range("I15").Select
+                Call Bring_Information
+            Else
+                For i = 24 To 31
+                    Worksheets("Control de Etapas").Rows(i).Hidden = False
+                Next
+                Range("G20:M31").Select
+                Selection.ClearContents
+                Range("I15").Select
+                Call Bring_Information
+            End If
+        End If
+    End If
+End If
+End Sub
+
+'Macro de Bring_Information
+'Se elimina el keyword de Private para que sea una clase bastante ksual
+
+Sub Bring_Information()
+conc = Worksheets("Control de Etapas").Range("K13").Value
+'Actualizando las observaciones por actividad
+For i = 2 To 1000
+    If conc = Worksheets("BD Proyecto").Cells(i, 1).Value Then
+        k = 20
+        For j = 17 To 22 Step 1
+            Worksheets("Control de Etapas").Range("l" & k) = Worksheets("BD Proyecto").Cells(i, j).Value
+        Next
+        k = 20
+        For j = 23 To 28 Step 1
+            Worksheets("Control de Etapas").Range("G" & k) = Worksheets("BD Proyecto").Cells(i, j).Value
+            k = k + 2
+            'm = MsgBox("Si vas por good way", vbOKOnly, "Yuju")
+        Next
+        Worksheets("Control de Etapas").Range("D38") = Worksheets("BD Proyecto").Cells(i, 30).Value
+    End If
+Next
+End Sub
+
+
+
+
+
+
+'Este que sigue es uno más personalizado, es el botón de actualizar
+Private Sub UpdateInfo_Click()
+If Worksheets("Control de Etapas").Range("C15").Value = "" Or Worksheets("Control de Etapas").Range("I15").Value = "" Then
+    m = MsgBox("Por favor seleccione una etapa válida", vbOKOnly, "Grave Error")
+    conc = Worksheets("Control de Etapas").Range("K13").Value
+    'Actualizando las observaciones por actividad
+Else
+    a = MsgBox("¿Completamente seguro de que quiere actualizar esta etapa?", vbYesNo, "OJO!!!")
+    If a = vbYes Then
+    For i = 2 To 1000
+        If conc = Worksheets("BD Proyecto").Cells(i, 1).Value Then
+            k = 20
+            For j = 17 To 22 Step 1
+                Worksheets("BD Proyecto").Cells(i, j) = Worksheets("Control de Etapas").Range("l" & k).Value
+            Next
+            k = 20
+            For j = 23 To 28 Step 1
+                Worksheets("BD Proyecto").Cells(i, j) = Worksheets("Control de Etapas").Range("G" & k).Value
+                k = k + 2
+                'm = MsgBox("Si vas por good way", vbOKOnly, "Yuju")
+            Next
+            Worksheets("Control de Etapas").Range("D38") = Worksheets("BD Proyecto").Cells(i, 30).Value
+        End If
+    Next
+    End If
+End If
+End Sub
